11b88388b971e41d6c8530dd9c8d02859fed4512
manoelourenzo <142229129+manoelourenzo@users.noreply.github.com>
2024-09-10 10:46:16 -0300

    Mudança no atributo CPF

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db import models
 
 class Usuario(models.Model):
-    cpf = models.IntegerField(max_length=11, unique=True)
+    cpf = models.IntegerField(unique=True)
     nome = models.CharField(max_length=100)
     data_nascimento = models.DateField()
 
